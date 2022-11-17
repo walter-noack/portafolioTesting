7399f75840f0e82441f61436896994dcb7de461b
walter-noack <wnoack@gmail.com>
2022-11-17 11:21:31 -0300

    actualización pokedex

--- database/config.js
+++ database/config.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const stringConnection = async() => {
     try {
-        await mongoose.connect(process.env.DB_CNN)
+        await mongoose.connect('mongodb+srv://admin:adminPortafolio@clusterportafolio.45vxokl.mongodb.net/test')
         console.log('Conectado a la Pokedex')
     } catch(error) {
         console.log(error);
